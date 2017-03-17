67defc8782801a71c9f4a7ada7d28b1d0f016d9a
Christof Leuenberger <chris@chlab.ch>
2017-03-17 18:16:21 +0100

    stop walk animation when player jumps

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -8,19 +8,27 @@ export default class extends Phaser.Sprite {
     this.animations.add('walk')
 
     this.jumpSound = this.game.add.audio('jump')
+    this.scale.setTo(0.3)
   }
 
   /**
    * Setup player physics and animations
    */
   enable() {
+    this.enabled = true
     this.game.physics.arcade.enable(this)
     this.body.gravity.y = 3000
     this.body.velocity.x = 250
-    this.animations.play('walk', 15, true)
+    this.startWalking()
   }
 
   update () {
+    if (this.enabled && this.body.blocked.down) {
+      this.startWalking()
+    }
+    else {
+      this.stopWalking()
+    }
   }
 
   /**
@@ -30,6 +38,21 @@ export default class extends Phaser.Sprite {
     if (this.body.blocked.down) {
       this.jumpSound.play()
       this.body.velocity.y -= 900
+    }
+  }
+
+  startWalking() {
+    if (!this.walking) {
+      this.animations.play('walk', 15, true)
+      this.walking = true
+    }
+  }
+
+  stopWalking() {
+    if (this.walking) {
+      this.animations.stop('walk')
+      this.walking = false
+      this.frame = 4
     }
   }
 }
