9742bd1eb409fffb0125fa1c00c6d5d81a7999d3
Christof Leuenberger <chris@chlab.ch>
2017-03-17 15:58:04 +0100

    added keys, layers etc.

--- src/states/Game.js
+++ src/states/Game.js
@@ -15,22 +15,34 @@ export default class extends Phaser.State {
     banner.smoothed = false
     banner.anchor.setTo(0.5)
 
-    this.game.cursors = this.game.input.keyboard.createCursorKeys()
+    this.game.stage.backgroundColor = '#222222'
+    this.map = this.game.add.tilemap('platform')
+    this.map.addTilesetImage('platformer_tiles', 'tiles')
 
-    this.player = new Player({
-      game: this.game,
-      x: 100,
-      y: 100,
-      asset: 'player'
+    this.backgroundLayer = this.map.createLayer('layer_1')
+    // this.blockedLayer = this.map.createLayer('blockedLayer')
+    // this.map.setCollisionBetween(1, 200000, true, 'blockedLayer', true)
+    this.backgroundLayer.resizeWorld()
+
+    // define game keys
+    this.game.keys = this.keys = this.game.input.keyboard.addKeys({
+      space: Phaser.KeyCode.SPACEBAR,
+      right: Phaser.KeyCode.RIGHT
     })
+
+    // add player
+    this.player = new Player({ game: this.game, x: 100, y: 300, asset: 'player' })
+    setTimeout(() => {
+      this.player.enable()
+    }, 1000)
+    this.game.camera.follow(this.player)
   }
 
   update () {
+    this.game.physics.arcade.collide(this.player, this.ground)
+    this.game.physics.arcade.collide(this.player, this.blockedLayer, this.playerHit, null, this)
   }
 
   render () {
-    /*if (__DEV__) {
-      this.game.debug.spriteInfo(this.player, 32, 32)
-    }*/
   }
 }
