1696e13368f1413e623720d11cd92b188790acc6
Christof Leuenberger <chris@chlab.ch>
2017-03-17 17:48:25 +0100

    changed loading message

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -19,8 +19,8 @@ export default class extends Phaser.State {
     let text = this.add.text(
       this.world.centerX,
       this.world.centerY,
-      'loading fonts',
-      { font: '16px Arial', fill: '#dddddd', align: 'center' })
+      'loading'.toUpperCase(),
+      { font: '16px Arial', fill: '#000000', align: 'center' })
     text.anchor.setTo(0.5, 0.5)
 
     this.load.image('loaderBg', './assets/images/loader-bg.png')
